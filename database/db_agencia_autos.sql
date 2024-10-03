f959bebee7d268549d38ce7be4aee8cefa12a0e8
Mayra Andrea Denot <122070858+MayDenot@users.noreply.github.com>
2024-10-03 09:57:13 -0300

    Actualización de base de datos

--- database/db_agencia_autos.sql
+++ database/db_agencia_autos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-09-2024 a las 00:01:55
+-- Tiempo de generación: 03-10-2024 a las 14:54:35
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `alquileres` (
   `ID` int(11) NOT NULL,
   `ID_Vehiculo` int(11) NOT NULL,
+  `ID_Usuario` int(11) NOT NULL,
   `Fecha_de_entrega` date NOT NULL,
   `Fecha_de_vencimiento` date NOT NULL,
   `Precio` double NOT NULL
@@ -39,9 +40,29 @@ CREATE TABLE `alquileres` (
 -- Volcado de datos para la tabla `alquileres`
 --
 
-INSERT INTO `alquileres` (`ID`, `ID_Vehiculo`, `Fecha_de_entrega`, `Fecha_de_vencimiento`, `Precio`) VALUES
-(1, 1, '2024-09-14', '2024-09-21', 2500),
-(4, 2, '2024-09-17', '2024-09-25', 3000);
+INSERT INTO `alquileres` (`ID`, `ID_Vehiculo`, `ID_Usuario`, `Fecha_de_entrega`, `Fecha_de_vencimiento`, `Precio`) VALUES
+(5, 1, 2, '2024-09-04', '2024-09-11', 6000),
+(7, 5, 2, '2024-10-02', '2024-10-09', 4500),
+(8, 5, 2, '2024-10-02', '2024-10-16', 10000);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `ID_Usuario` int(11) NOT NULL,
+  `Usuario` varchar(250) NOT NULL,
+  `Contraseña` char(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`ID_Usuario`, `Usuario`, `Contraseña`) VALUES
+(2, 'webadmin', '$2y$10$coasoo4/KT2t88RTP6WHEeYHQ1YFNRsEydcMIwsL.84TWMLaIaHmm');
 
 -- --------------------------------------------------------
 
@@ -76,7 +97,15 @@ INSERT INTO `vehiculos` (`ID_Vehiculo`, `Patente`, `Modelo`, `Marca`, `Año_de_M
 --
 ALTER TABLE `alquileres`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `fk_id_vehiculo` (`ID_Vehiculo`);
+  ADD KEY `fk_id_vehiculo` (`ID_Vehiculo`),
+  ADD KEY `fk_id_usuario` (`ID_Usuario`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID_Usuario`),
+  ADD UNIQUE KEY `Usuario` (`Usuario`);
 
 --
 -- Indices de la tabla `vehiculos`
@@ -93,7 +122,13 @@ ALTER TABLE `vehiculos`
 -- AUTO_INCREMENT de la tabla `alquileres`
 --
 ALTER TABLE `alquileres`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID_Usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `vehiculos`
@@ -109,6 +144,7 @@ ALTER TABLE `vehiculos`
 -- Filtros para la tabla `alquileres`
 --
 ALTER TABLE `alquileres`
+  ADD CONSTRAINT `alquileres_ibfk_1` FOREIGN KEY (`ID_Usuario`) REFERENCES `usuarios` (`ID_Usuario`),
   ADD CONSTRAINT `fk_id_vehiculo` FOREIGN KEY (`ID_Vehiculo`) REFERENCES `vehiculos` (`ID_Vehiculo`);
 COMMIT;
 
